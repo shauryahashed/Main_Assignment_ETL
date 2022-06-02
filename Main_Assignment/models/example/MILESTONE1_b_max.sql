07bf647ff91770d39ec471ece8d12808fd442846
Shaurya Singh <shausingh@deloitte.com>
2022-06-02 10:10:38 +0530

    First Commit

---
+++ Main_Assignment/models/example/MILESTONE1_b_max.sql
@@ -0,0 +1,5 @@
+select nav_date, t4.code as code, t4.maximum from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" t3 join
+(select t1.code as code, max(t2.nav) as maximum from "ANALYTICS"."MAINASSIGNMENT"."MUTUALFUND" t1 join
+"ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" t2
+ on t1.code = t2.code group by t1.code) t4
+where t3.code = t4.code and t3.nav = t4.maximum
