de0d7ce517095753815200050aa26d0f6fcfc3ca
Shaurya Singh <shausingh@deloitte.com>
2022-06-02 13:12:23 +0530

    Added the documentation file

--- Main_Assignment/models/example/MILESTONE1_a.sql
+++ Main_Assignment/models/example/MILESTONE1_a.sql
@@ -1,2 +1,2 @@
-select month(nav_date) as nav_month ,code, avg(nav) as nav_average,
-avg(repurchase_price) as repurchase_average, avg(sale_price) as sale_average from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" group by code, nav_month
+select month(nav_date) as nav_month ,code, avg(nav) as nav_av,
+avg(repurchase_price) as rep_av, avg(sale_price) as sa_av from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" group by code, nav_month
--- Main_Assignment/models/example/Milestone2_3.sql
+++ Main_Assignment/models/example/Milestone2_3.sql
@@ -1,3 +1,3 @@
-select code, max(nav_date) as maximum from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" group by code
+select code, max(nav_date) as max from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY" group by code
 
-having max(nav_date) < (select max(nav_date) as maximum from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY")
+having max(nav_date) < (select max(nav_date) as max from "ANALYTICS"."MAINASSIGNMENT"."NAVHISTORY")
